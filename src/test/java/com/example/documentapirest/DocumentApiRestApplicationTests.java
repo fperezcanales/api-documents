c63af0040df839364aaac0b654712c1d535db289
Fernando Perez <fperez.canales@gmail.com>
2020-04-19 14:11:22 -0400

    First commit

---
+++ src/test/java/com/example/documentapirest/DocumentApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.documentapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
